b68363f77fa801fb0c1299ee4b5d5152cacabe16
ganesh bonangi <ganesh.bonangi@gmail.com>
2014-07-24 00:50:00 +0530

    rendered html in webpage

--- scripts/services/questionsService.js
+++ scripts/services/questionsService.js
@@ -4,13 +4,22 @@ app.factory("QuestionsService",function($rootScope,$sce){
 		setQestionData:function(){
 			$rootScope.total_questions=$($rootScope.xmlData).find("section[name='"+$rootScope.section+"'] questioninfo").length;
 			var questionNode=$($rootScope.xmlData).find("section[name='"+$rootScope.section+"'] questioninfo")[$rootScope.index];
-			$rootScope.question_information=$sce.trustAsHtml($(questionNode).find("info").html());
-			$rootScope.question=$sce.trustAsHtml($(questionNode).find("question").html());
+			if($.parseHTML($(questionNode).find("info").html()))
+				$rootScope.question_information=$sce.trustAsHtml($.parseHTML($(questionNode).find("info").html())[0].nodeValue);
+			else
+				$rootScope.question_information="";
+			if($.parseHTML($(questionNode).find("question").html()))
+				$rootScope.question=$sce.trustAsHtml($.parseHTML($(questionNode).find("question").html())[0].nodeValue);
+			else
+				$rootScope.question="";
 			var options=$(questionNode).find("option");
 			$rootScope.options=new Array();
 			$rootScope.ans=$(questionNode).find("answer").text();
 			for(var i=0;i<options.length;i++){
-				$rootScope.options.push($sce.trustAsHtml($(options[i]).html()));
+				if($.parseHTML($(options[i]).html()))
+					$rootScope.options.push($sce.trustAsHtml($.parseHTML($(options[i]).html())[0].nodeValue));
+				else
+					$rootScope.options.push("");
 			}	
 		},
 		getNextSection:function(){
